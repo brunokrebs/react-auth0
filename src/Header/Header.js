750d98d18fc1b386c5505eacad549a87e9ff9791
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-28 21:38:56 -0200

    Starting Auth0 integration

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Auth from '../Auth/Auth.js';
 import './Header.css';
 
 class Header extends Component {
+  signIn() {
+    console.log('good to go');
+    Auth.login();
+  }
+
   render() {
     return (
       <div className="app-header">
         <h1>React App Secured with Auth0</h1>
         <div className="app-header-links">
+          <button onClick={this.signIn}>
+            Sign In
+          </button>
           <Link to="/contacts" className="dashed">Contacts</Link>
         </div>
       </div>
