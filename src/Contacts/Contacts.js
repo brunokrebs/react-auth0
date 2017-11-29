4ae76fcc2d27626b3a38f5aaae851d0efa330001
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-28 23:10:58 -0200

    issuing requests to backend

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import './Contacts.css';
+import axios from 'axios';
 
 class Contacts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {contacts: []};
+  }
+
+  componentDidMount() {
+    const self = this;
+    const config = {
+      url: 'http://localhost:3001/contacts',
+      headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
+    };
+
+    axios(config).then(function (response) {
+      self.setState({contacts: response.data});
+    }).catch(console.log);
+  }
+
   render() {
     return (
       <table className="contacts-table">
-        <tr><th>Name</th><th>Phone</th></tr>
-        <tr><td>Bruno Krebs</td><td>(51) 98121 8604</td></tr>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+        </tr>
+        </thead>
+        <tbody>
+        {this.state.contacts.map((item, index) => (
+          <tr key={index}>
+            <td>{item.name}</td>
+            <td>{item.phone}</td>
+          </tr>
+        ))}
+        </tbody>
       </table>
     );
   }
