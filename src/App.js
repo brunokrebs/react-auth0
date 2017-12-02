673fd2c4f0e3ed095bab02e9f0ce942c7e0afffa
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-02 17:17:56 -0200

    starting migration

--- src/App.js
+++ src/App.js
@@ -4,16 +4,38 @@ import Header from './Header/Header.js';
 import Contacts from './Contacts/Contacts.js';
 import Home from './Home/Home.js';
 import Callback from "./Auth/Callback";
-import Auth from './Auth/Auth.js';
+import * as Auth0 from 'auth0-web';
 
 const handleAuthentication = (nextState) => {
-  if (/access_token|id_token|error/.test(nextState.location.hash)) {
-    Auth.handleAuthentication(nextState.history);
-  }
+  Auth0.handleAuthCallback();
+  // Auth0.subscribe((signedIn) => {
+  //   const destiny = signedIn ? '/contacts' : '/';
+  //   nextState.history.replace(destiny);
+  // });
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    Auth0.configure({
+      domain: 'bk-samples.auth0.com',
+      audience: 'https://contacts.digituz.com.br',
+      clientID: '8a7myyLd6leG0HbOhMPtLaSgZ2itD3gK',
+      redirectUri: 'http://localhost:3000/callback',
+      responseType: 'token id_token',
+      scope: 'openid get:contacts post:contacts delete:contacts'
+    });
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount App.js');
+    // this.setState({isAuthenticated: Auth0.isAuthenticated()});
+  }
+
   render() {
+    // console.log('------ render App.js');
+    // console.log(this.state.isAuthenticated);
+    // console.log('------ render App.js');
     return (
       <div className="app">
         <Route path="/" component={Header}/>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,32 +1,18 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import Auth from '../Auth/Auth.js';
 import './Header.css';
+import * as Auth0 from 'auth0-web';
 
 class Header extends Component {
-  constructor() {
-    super();
-    this.signIn = this.signIn.bind(this);
-    this.signOut = this.signOut.bind(this);
-  }
-
-  signIn() {
-    Auth.login();
-  }
-
-  signOut() {
-    Auth.logout(this.props.history);
-  }
-
   render() {
-    const authenticated = Auth.isAuthenticated();
+    const authenticated = Auth0.isAuthenticated();
     return (
       <div className="app-header">
         <h1>React App Secured with Auth0</h1>
         <div className="app-header-links">
-          {!authenticated && <button onClick={this.signIn}>Sign In</button>}
+          {!authenticated && <button onClick={Auth0.signIn}>Sign In</button>}
           {authenticated && <Link to="/contacts" className="dashed">Contacts</Link>}
-          {authenticated && <button onClick={() => (this.signOut())}>Sign Out</button>}
+          {authenticated && <button onClick={() => (Auth0.signOut())}>Sign Out</button>}
         </div>
       </div>
     );
