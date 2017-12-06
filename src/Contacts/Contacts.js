e0020095cd59d18ed167fe024a863520d8dd5668
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-06 02:50:59 -0200

    Adding edit and remove buttons to table. Actually, I think that this would look better if the buttons were passed through props. The solution in this case would be add one/more Button per row.
    Anyway, good for now.

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -2,14 +2,38 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Panel from "../DOMElements/Panel/Panel";
 import Table from "../DOMElements/Table/Table";
+import {withRouter} from 'react-router-dom';
 
 class Contacts extends Component {
   constructor(props) {
     super(props);
     this.state = {contacts: []};
+    this.editContact = this.editContact.bind(this);
+    this.removeContact = this.removeContact.bind(this);
+    this.loadContactsList = this.loadContactsList.bind(this);
   }
 
   componentDidMount() {
+    this.loadContactsList();
+  }
+
+  editContact(id) {
+    this.props.history.push('/contacts/' + id);
+  }
+
+  removeContact(id) {
+    const self = this;
+    const config = {
+      method: 'delete',
+      url: 'http://auth0-wildcard.digituz.com.br/contacts/' + id,
+      headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
+    };
+    axios(config).then(function () {
+      self.loadContactsList();
+    }).catch(console.log);
+  }
+
+  loadContactsList() {
     const self = this;
     const config = {
       url: 'http://auth0-wildcard.digituz.com.br/contacts',
@@ -23,17 +47,17 @@ class Contacts extends Component {
 
   render() {
     const headers = [
-      { key: 'name', text: 'Name' },
-      { key: 'phone', text: 'Phone' }
+      {key: 'name', text: 'Name'},
+      {key: 'phone', text: 'Phone'}
     ];
     const rows = this.state.contacts;
     return (
       <Panel>
         <h2>List of Contacts</h2>
-        <Table headers={headers} rows={rows} />
+        <Table headers={headers} rows={rows} onEditClick={this.editContact} onRemoveClick={this.removeContact}/>
       </Panel>
     );
   }
 }
 
-export default Contacts;
+export default withRouter(props => <Contacts {...props} />);
--- src/DOMElements/Table/Table.js
+++ src/DOMElements/Table/Table.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Table.css';
+import Button from "../Button/Button";
 
 class Table extends Component {
   render() {
     const headers = this.props.headers || [];
     const rows = this.props.rows || [];
+    const {onEditClick, onRemoveClick} = this.props;
+    const showTableActions = onEditClick || onRemoveClick;
     return (
       <table className='react-auth0'>
         <thead>
-          <tr>
-            {headers.map((header, idx) => (
-              <th key={idx}>{header.text}</th>
-            ))}
-          </tr>
+        <tr>
+          {headers.map((header, idx) => (
+            <th key={idx}>{header.text}</th>
+          ))}
+          {showTableActions && <th/>}
+        </tr>
         </thead>
         <tbody>
         {rows.map((row, idx) => (
@@ -20,6 +24,10 @@ class Table extends Component {
             {headers.map((header, idx) => (
               <td key={idx}>{row[header.key]}</td>
             ))}
+            {showTableActions && <td className='table-actions'>
+              {onEditClick && <Button onClick={() => (this.props.onEditClick(row._id))} text='Edit'/>}
+              {onRemoveClick && <Button onClick={() => (this.props.onRemoveClick(row._id))} text='Remove'/>}
+            </td>}
           </tr>
         ))}
         </tbody>
