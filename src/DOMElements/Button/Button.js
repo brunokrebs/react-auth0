0651b26f41b6af713fd3b3f49291eee692fe25fb
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-06 01:40:16 -0200

    making Buttons accept className enhancements through properties;

--- src/DOMElements/Button/Button.js
+++ src/DOMElements/Button/Button.js
@@ -4,8 +4,9 @@ import '../Shadow/Shadow.css';
 
 class Button extends Component {
   render() {
+    const className = 'react-auth0 shadow default-font-size ' + this.props.className;
     return (
-      <button className='react-auth0 shadow default-font-size'
+      <button className={className}
               onClick={this.props.onClick}>
         {this.props.text}
       </button>
