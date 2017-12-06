c84ab46dd5b3d555709ecee4a2f75e299c405aac
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-06 02:58:16 -0200

    moving method definitions down below

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -13,37 +13,11 @@ class Contacts extends Component {
     this.loadContactsList = this.loadContactsList.bind(this);
   }
 
-  componentDidMount() {
-    this.loadContactsList();
-  }
-
-  editContact(id) {
-    this.props.history.push('/contacts/' + id);
-  }
-
-  removeContact(id) {
-    const self = this;
-    const config = {
-      method: 'delete',
-      url: 'http://auth0-wildcard.digituz.com.br/contacts/' + id,
-      headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
-    };
-    axios(config).then(function () {
-      self.loadContactsList();
-    }).catch(console.log);
-  }
-
-  loadContactsList() {
-    const self = this;
-    const config = {
-      url: 'http://auth0-wildcard.digituz.com.br/contacts',
-      headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
-    };
-
-    axios(config).then(function (response) {
-      self.setState({contacts: response.data});
-    }).catch(console.log);
-  }
+  // methods
+  loadContactsList = loadContactsList;
+  editContact = editContact;
+  removeContact = removeContact;
+  componentDidMount = loadContactsList;
 
   render() {
     const headers = [
@@ -61,3 +35,31 @@ class Contacts extends Component {
 }
 
 export default withRouter(props => <Contacts {...props} />);
+
+function loadContactsList() {
+  const self = this;
+  const config = {
+    url: 'http://auth0-wildcard.digituz.com.br/contacts',
+    headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
+  };
+
+  axios(config).then(function (response) {
+    self.setState({contacts: response.data});
+  }).catch(console.log);
+}
+
+function editContact(id) {
+  this.props.history.push('/contacts/' + id);
+}
+
+function removeContact(id) {
+  const self = this;
+  const config = {
+    method: 'delete',
+    url: 'http://auth0-wildcard.digituz.com.br/contacts/' + id,
+    headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
+  };
+  axios(config).then(function () {
+    self.loadContactsList();
+  }).catch(console.log);
+}
