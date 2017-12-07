1731658117a0d871781192b80fe0e46fd6fd0e05
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-07 18:02:24 -0200

    Contact using EntityForm

--- src/Contacts/Contact.js
+++ src/Contacts/Contact.js
@@ -1,87 +1,42 @@
 import React, {Component} from 'react';
-import Panel from "../DOMElements/Panel/Panel";
-import LabeledInput from "../DOMElements/LabeledInput/LabeledInput";
-import Button from "../DOMElements/Button/Button";
 import '../DOMElements/Margin/Margin.css';
-import axios from 'axios';
 import {withRouter} from 'react-router-dom';
+import EntityForm from '../EntityForm/EntityForm';
 
 class Contact extends Component {
   constructor(props) {
     super(props);
     let {contactId} = this.props.match.params;
     this.state = {
-      contactId,
       contact: {
+        _id: (contactId === 'new' ? null : contactId),
         name: '',
         phone: ''
       }
     };
-    this.handleChange = handleChange.bind(this);
-    this.onClick = onClick.bind(this);
-    this.componentDidMount = componentDidMount.bind(this);
+    this.refreshContact = this.refreshContact.bind(this);
+  }
+
+  refreshContact(contact) {
+    this.setState({contact});
   }
 
   render() {
+    const title = 'Contact Form';
+    const entityName = 'contacts';
+    const inputs = [
+      {field: 'name', label: 'Name:', placeholder: 'Contact Name', type: 'text'},
+      {field: 'phone', label: 'Phone:', placeholder: '+55 51 982234343', type: 'text'},
+      {field: 'email', label: 'Email:', placeholder: 'someone@somewhere.com', type: 'text'},
+      {field: 'heritage', label: 'Heritage:', placeholder: '50,00', type: 'currency'}
+    ];
+
     return (
-      <Panel>
-        <h2>Contact Form</h2>
-        <LabeledInput label="Name:" placeholder="Contact Name"
-                      value={this.state.contact.name} onChange={this.handleChange('name')}/>
-
-        <LabeledInput label="Phone:" placeholder="+55 51 982234343"
-                      value={this.state.contact.phone} onChange={this.handleChange('phone')}/>
-
-        <LabeledInput label="Email:" placeholder="someone@somewhere.com"
-                      value={this.state.contact.email} onChange={this.handleChange('email')}/>
-
-        <LabeledInput label="Heritage:" placeholder="50,00" type='currency'
-                      value={this.state.contact.heritage} onChange={this.handleChange('heritage')}/>
-        <Button onClick={() => (this.onClick())} text="Save" className='margin-top'/>
-      </Panel>
+      <EntityForm entity={this.state.contact} entityName={entityName}
+                  title={title} refreshEntity={this.refreshContact}
+                  inputs={inputs} />
     );
   }
 }
 
 export default withRouter(props => <Contact {...props}/>);
-
-function componentDidMount() {
-  const self = this;
-  const {contactId} = this.state;
-
-  // loading contact details
-  if (contactId !== 'new') {
-    const config = {
-      url: process.env.REACT_APP_FLEX_REST + '/contacts/' + contactId,
-      headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
-    };
-    axios(config).then(function (response) {
-      self.setState({contact: response.data});
-    }).catch(console.log);
-  }
-}
-
-function onClick() {
-  const self = this;
-  const contactId = self.state.contactId === 'new' ? '' : self.state.contactId;
-  const config = {
-    method: contactId ? 'put' : 'post',
-    url: process.env.REACT_APP_FLEX_REST + '/contacts/' + contactId,
-    data: this.state.contact,
-    headers: {'Authorization': 'Bearer ' + localStorage.getItem('access_token')}
-  };
-  axios(config).then(function () {
-    self.props.history.push('/contacts');
-  }).catch(console.log);
-}
-
-function handleChange(property) {
-  return (event) => {
-    this.setState({
-      contact: {
-        ...this.state.contact,
-        [property]: event.target.value
-      }
-    });
-  }
-}
