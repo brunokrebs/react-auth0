4f5e25a48cbe19d907875fe8a5f093c4eb50cab9
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-20 17:04:16 -0200

    setting Contacts scopes by default;

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
       responseType: 'token id_token',
-      scope: 'openid get:diagrams post:diagrams put:diagrams delete:diagrams'
+      scope: 'openid get:contacts post:contacts put:contacts delete:contacts'
     });
   }
 
