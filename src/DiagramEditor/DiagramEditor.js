3d20c15d6a8771548821c1f302a419c5ee993e68
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-20 04:15:00 -0200

    reorganizing methods

--- src/DiagramEditor/DiagramEditor.js
+++ src/DiagramEditor/DiagramEditor.js
@@ -11,68 +11,16 @@ class DiagramEditor extends Component {
 
   constructor(props) {
     super(props);
-    this.selectElement = this.selectElement.bind(this);
-    this.updateSize = this.updateSize.bind(this);
-    this.addRectangles = this.addRectangles.bind(this);
-    this.addCircle = this.addCircle.bind(this);
-    this.addAuth0 = this.addAuth0.bind(this);
-    this.saveDiagram = this.saveDiagram.bind(this);
+    this.selectElement = selectElement.bind(this);
+    this.updateSize = updateSize.bind(this);
+    this.addRectangles = addRectangles.bind(this);
+    this.addCircle = addCircle.bind(this);
+    this.addAuth0 = addAuth0.bind(this);
+    this.saveDiagram = saveDiagram.bind(this);
     this.state = {
       elements: [],
       size: 0
     }
-  }
-
-  selectElement(event) {
-    this.selectedElement = event.target;
-  }
-
-  updateSize(event) {
-    this.setState({
-      size: event.target.value
-    });
-  }
-
-  addRectangles() {
-    const elements = this.state.elements;
-    const elementId = DiagramEditor.generateElementId('rect');
-    elements.push(
-      <Rectangles elementId={elementId} key={elementId} />
-    );
-    this.setState({
-      elements
-    });
-  }
-
-  addCircle() {
-    const elements = this.state.elements;
-    const elementId = DiagramEditor.generateElementId('circle');
-    elements.push(
-      <Circles elementId={elementId} key={elementId} />
-    );
-    this.setState({
-      elements
-    });
-  }
-
-  addAuth0() {
-    const elements = this.state.elements;
-    const elementId = DiagramEditor.generateElementId('auth0');
-    elements.push(
-      <Auth0Logo elementId={elementId} key={elementId} />
-    );
-    this.setState({
-      elements
-    });
-  }
-
-  saveDiagram() {
-    console.log(this.state.elements);
-  }
-
-  static generateElementId(elementType) {
-    const time = (new Date()).getTime();
-    return `${elementType}-${time}`;
   }
 
   render() {
@@ -91,3 +39,55 @@ class DiagramEditor extends Component {
 }
 
 export default DiagramEditor;
+
+function selectElement(event) {
+  this.selectedElement = event.target;
+}
+
+function updateSize(event) {
+  this.setState({
+    size: event.target.value
+  });
+}
+
+function addRectangles() {
+  const elements = this.state.elements;
+  const elementId = generateElementId('rect');
+  elements.push(
+    <Rectangles elementId={elementId} key={elementId} />
+  );
+  this.setState({
+    elements
+  });
+}
+
+function addCircle() {
+  const elements = this.state.elements;
+  const elementId = generateElementId('circle');
+  elements.push(
+    <Circles elementId={elementId} key={elementId} />
+  );
+  this.setState({
+    elements
+  });
+}
+
+function addAuth0() {
+  const elements = this.state.elements;
+  const elementId = generateElementId('auth0');
+  elements.push(
+    <Auth0Logo elementId={elementId} key={elementId} />
+  );
+  this.setState({
+    elements
+  });
+}
+
+function saveDiagram() {
+  console.log(this.state.elements);
+}
+
+function generateElementId(elementType) {
+  const time = (new Date()).getTime();
+  return `${elementType}-${time}`;
+}
