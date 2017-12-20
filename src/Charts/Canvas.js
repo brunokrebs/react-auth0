253c53b03b19ebc49d9ea40214f055f88cb801bc
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-20 04:06:34 -0200

    removing unused imports

--- src/Charts/Canvas.js
+++ src/Charts/Canvas.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
-import Circles from './Circles/Circles';
 import './Canvas.css';
-import Auth0Logo from "./Auth0/Auth0Logo";
-import Rectangles from "./Rectangles/Rectangles";
 
 class Canvas extends Component {
   render() {
