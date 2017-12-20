4969e21df08ee08dfa833a38854a134b67df0a9d
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-20 10:59:05 -0200

    making shapes completely dumb;

--- src/Charts/Circles/Circles.js
+++ src/Charts/Circles/Circles.js
@@ -1,23 +1,12 @@
 import React, {Component} from 'react';
 import './Circles.css'
-import {selectElement, moveElement, releaseElement} from "../Movements/Movements";
 
 class Circles extends Component {
-  constructor(props) {
-    super(props);
-    this.selectElement = selectElement.bind(this);
-    this.moveElement = moveElement.bind(this);
-    this.releaseElement = releaseElement.bind(this);
-    this.updateElementLocation = props.updateElementLocation;
-  }
-
   render() {
     const matrix = `matrix(${this.props.matrix.join(' ')})`;
     return (
-      <circle id={this.props.elementId} className='react-auth0' cx="50" cy="50" r="40" transform={matrix}
-              onMouseDown={(event) => (this.selectElement(event))}
-              onMouseMove={(event) => (this.moveElement(event))}
-              onMouseUp={(event) => (this.releaseElement(event))}/>
+      <circle id={this.props.elementId} className='react-auth0' transform={matrix}
+              cx='50' cy='50' r='40'/>
     );
   }
 }
--- src/Charts/Rectangles/Rectangles.js
+++ src/Charts/Rectangles/Rectangles.js
@@ -1,24 +1,12 @@
 import React, {Component} from 'react';
-import {selectElement, moveElement, releaseElement} from "../Movements/Movements";
 import './Rectangles.css';
 
 class Rectangles extends Component {
-  constructor(props) {
-    super(props);
-    this.selectElement = selectElement.bind(this);
-    this.moveElement = moveElement.bind(this);
-    this.releaseElement = releaseElement.bind(this);
-    this.updateElementLocation = props.updateElementLocation;
-  }
-
   render() {
     const matrix = `matrix(${this.props.matrix.join(' ')})`;
     return (
-      <rect id={this.props.elementId} className='react-auth0' x="300" y="10" width="100" height="100"
-            onMouseDown={(event) => (this.selectElement(event))}
-            onMouseMove={(event) => (this.moveElement(event))}
-            onMouseUp={(event) => (this.releaseElement(event))}
-            transform={matrix}/>
+      <rect id={this.props.elementId} className='react-auth0' transform={matrix}
+            x='300' y='10' width='100' height='100' />
     );
   }
 }
